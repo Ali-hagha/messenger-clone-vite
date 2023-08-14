a3153d4d429af0cc9ed5f5e35fc645932677a1c2
Ali home pc <ali.haghayegh@gmail.com>
2023-08-14 09:45:27 +0330

    fix bug: users could create multiple conversations with same person.

--- src/components/users/UserBox.tsx
+++ src/components/users/UserBox.tsx
@@ -1,8 +1,8 @@
-import { MouseEvent, useState } from 'react';
-import { PbUser } from '../../types/types';
-import { useNavigate } from 'react-router-dom';
-import Avatar from '../ui/Avatar';
-import { pocketbase } from '../../lib/pocketbase';
+import { MouseEvent, useState } from "react";
+import { PbUser } from "../../types/types";
+import { useNavigate } from "react-router-dom";
+import Avatar from "../ui/Avatar";
+import { pocketbase } from "../../lib/pocketbase";
 
 interface Props {
   user: PbUser;
@@ -16,20 +16,22 @@ const UserBox = ({ user }: Props) => {
   const currentUser = pocketbase.authStore.model;
 
   const handleClick = async (
-    e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
   ) => {
     e.preventDefault();
     setIsLoading(true);
 
-    const existingChats = await pocketbase.collection('chats').getFullList({
-      sort: '-created',
+    const existingChats = await pocketbase.collection("chats").getFullList({
+      sort: "-created",
       filter: `users ~ "${currentUser?.id}" && users ~ "${user.id}"`,
     });
 
     const singleChat = existingChats[0];
 
     if (singleChat && singleChat.id) {
+      setIsLoading(false);
       navigate(`../chats/${singleChat.id}`);
+      return;
     }
 
     const data = {
@@ -38,23 +40,22 @@ const UserBox = ({ user }: Props) => {
       users: [currentUser?.id, user.id],
     };
 
-    const newChat = await pocketbase.collection('chats').create(data);
+    const newChat = await pocketbase.collection("chats").create(data);
 
+    setIsLoading(false);
     navigate(`../chats/${newChat.id}`);
   };
 
   return (
-    <>
-      <div
-        onClick={e => handleClick(e)}
-        className="mb-2 bg-white md:bg-transparent hover:bg-slate-200 text-gray-500 hover:text-gray-700 transition active:bg-slate-300 flex items-center justify-start p-3 rounded-xl cursor-pointer"
-      >
-        <Avatar user={user} />
-        <div className="ml-4 select-none text-sm font-semibold">
-          {user.name}
-        </div>
-      </div>
-    </>
+    <button
+      type="button"
+      disabled={isLoading}
+      onClick={(e) => handleClick(e)}
+      className="mb-2 flex w-full cursor-pointer items-center justify-start rounded-xl p-3 text-gray-500 transition enabled:hover:bg-slate-200 enabled:hover:text-gray-700 enabled:active:bg-slate-300 disabled:opacity-30 md:bg-transparent"
+    >
+      <Avatar user={user} />
+      <div className="ml-4 select-none text-sm font-semibold">{user.name}</div>
+    </button>
   );
 };
 
