dcb20717a7e1254fe02ff9a3383a2dc9161557b0
Ali home pc <ali.haghayegh@gmail.com>
2023-08-14 16:20:38 +0330

    update message seen array when message is in view

--- src/components/messages/MessageBubble.tsx
+++ src/components/messages/MessageBubble.tsx
@@ -1,8 +1,9 @@
-import clsx from 'clsx';
-import { format } from 'date-fns';
-import { PbMessage } from '../../types/types';
-import { pocketbase } from '../../lib/pocketbase';
-import Avatar from '../ui/Avatar';
+import clsx from "clsx";
+import { format } from "date-fns";
+import { PbMessage } from "../../types/types";
+import { pocketbase } from "../../lib/pocketbase";
+import Avatar from "../ui/Avatar";
+import { useInView } from "react-intersection-observer";
 
 interface Props {
   message: PbMessage;
@@ -10,32 +11,45 @@ interface Props {
 }
 
 const MessageBubble = ({ message, isLastMessage }: Props) => {
+  const { ref } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+    onChange: (inView) => {
+      if (inView && user.id && !message.seenBy.includes(user.id)) {
+        pocketbase
+          .collection("messages")
+          .update(message.id, { seenBy: [...message.seenBy, user.id] });
+      }
+    },
+  });
+
   const user = pocketbase.authStore.model!;
 
   const isOwnMessage = message.expand.sender.email === user.email;
 
   const messageSeenCount = (message.expand.seenBy || []).filter(
-    user => user.email !== message.expand.sender.email
+    (user) => user.email !== message.expand.sender.email,
   ).length;
 
   const isMessageSeen = messageSeenCount > 0;
 
   const seenList = (message.expand.seenBy || [])
-    .filter(user => user.email !== message.expand.sender.email)
-    .map(user => user.name)
-    .join(', ');
+    .filter((user) => user.email !== message.expand.sender.email)
+    .map((user) => user.name)
+    .join(", ");
 
   return (
     <div
+      ref={ref}
       className={clsx(
-        `p-2 flex flex-col justify-center`,
-        isOwnMessage ? 'items-end lg:items-start' : 'items-start'
+        `flex flex-col justify-center p-2`,
+        isOwnMessage ? "items-end lg:items-start" : "items-start",
       )}
     >
       <div
         className={clsx(
-          'flex',
-          isOwnMessage ? 'flex-row-reverse lg:flex-row' : 'flex-row'
+          "flex",
+          isOwnMessage ? "flex-row-reverse lg:flex-row" : "flex-row",
         )}
       >
         <div className="mx-3">
@@ -44,16 +58,16 @@ const MessageBubble = ({ message, isLastMessage }: Props) => {
         <div className="flex flex-col">
           <div
             className={clsx(
-              `py-2 px-4 rounded-2xl break-words flex flex-col`,
+              `flex flex-col break-words rounded-2xl px-4 py-2`,
               isOwnMessage
-                ? 'bg-gradient-to-r from-sky-700 to-sky-500 text-white max-w-xs md:max-w-sm rounded-se-sm lg:rounded-ss-sm lg:rounded-se-2xl selection:bg-white/30'
-                : 'bg-white text-gray-600 rounded-ss-sm selection:bg-sky-200 max-w-xs md:max-w-sm '
+                ? "max-w-xs rounded-se-sm bg-gradient-to-r from-sky-700 to-sky-500 text-white selection:bg-white/30 md:max-w-sm lg:rounded-se-2xl lg:rounded-ss-sm"
+                : "max-w-xs rounded-ss-sm bg-white text-gray-600 selection:bg-sky-200 md:max-w-sm ",
             )}
           >
             <p className="text-sm sm:text-base">{message.body}</p>
-            <div className="flex flex-row lg:flex-row-reverse mt-3 self-start lg:self-end">
-              <p className="text-xs select-none">
-                {format(new Date(message.created), 'p')}
+            <div className="mt-3 flex flex-row self-start lg:flex-row-reverse lg:self-end">
+              <p className="select-none text-xs">
+                {format(new Date(message.created), "p")}
               </p>
             </div>
           </div>
